d04623faa1dbafe62b58e9fa87e34d9e7d9bfbb0
Adam Kaoullas <Adam@Kaoullas.com>
2023-07-20 17:13:40 +0000

    Integration with DB

--- code-with-quarkus/src/main/java/org/acme/AccountResource.java
+++ code-with-quarkus/src/main/java/org/acme/AccountResource.java
@@ -2,12 +2,15 @@ package org.acme;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+import org.jboss.resteasy.reactive.RestQuery;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/transactions")
@@ -16,13 +19,28 @@ public class AccountResource {
     @GET
     @Path("/account/{acc_id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<AccountTransaction> transactions(@PathParam("acc_id") String accountId) {
+    public List<AccountTransaction> transactions(@PathParam("acc_id") String accountId,
+    @RestQuery String fromDateTime, @RestQuery String toDateTime) {
         System.out.println("Entering transactions");
+        System.out.println(fromDateTime);
+        System.out.println(toDateTime);
+       
         List<AccountTransaction> tranList = new ArrayList<AccountTransaction>();
-        AccountTransaction t1 = new AccountTransaction();
-        AccountTransaction t2 = new AccountTransaction();
-        tranList.add(t1);
-        tranList.add(t2);
         return tranList;
     }
+
+    @GET
+    @Path("{tx_id}")
+    public AccountTransaction getByTransactionId(@PathParam("tx_id") String transactionId) {
+        AccountTransaction transaction = AccountTransaction.find("tx_id", transactionId).firstResult();
+        if (transaction == null) {
+            throw new WebApplicationException("Transaction not found", 404);
+        }
+        return transaction;
+
+    }
+
+    
+
+
 }
--- code-with-quarkus/src/main/java/org/acme/AccountTransaction.java
+++ code-with-quarkus/src/main/java/org/acme/AccountTransaction.java
@@ -1,10 +1,51 @@
 package org.acme;
 
-public class AccountTransaction {
+import java.sql.Timestamp;
 
-    String id;
-    
-    String accountId;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-    
+@Entity
+@Table(name = "transactions", schema="transaction")
+public class AccountTransaction extends PanacheEntityBase {
+    enum Status {
+        pending,
+        rejected,
+        complete
+    }
+    enum TransactionType {
+        pos,
+        atm,
+        transfer,
+        online,
+        dd
+    }
+/* 
+    tx_id        | character varying(40)       |           | not null |         | extended |              |
+    acc_id       | character varying(40)       |           | not null |         | extended |              |
+    tx_ts        | timestamp without time zone |           | not null |         | plain    |              |
+    status       | character varying(40)       |           | not null |         | extended |              |
+    amount       | integer                     |           | not null |         | plain    |              |
+    merchantname | text                        |           |          |         | extended |              |
+    merchant_id  | character varying(40)       |           |          |         | extended |              |
+    tx_type      | character varying(40)       |           |          |         | extended |              |
+    tx_details   | text                        |           |          |         | extended |              |
+   */
+    @Id
+    //TODO: Convert to type UUID
+    public String tx_id;
+    public String acc_id;
+    public Timestamp tx_ts;
+
+    // TODO: Convert to enum
+    public String status;
+    public Integer amount;
+    public String merchantname;
+    public String merchant_id;
+    // TODO: Convert to enum
+    public String tx_type;
+    public String tx_details;
+
 }
