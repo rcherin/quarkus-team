078ffe4d7893537e8594faddab42679401c1be7b
Adam Kaoullas <Adam@Kaoullas.com>
2023-07-23 06:08:29 +0000

    Tidied up code, API fully working/implemented without Paging

--- code-with-quarkus/src/main/java/org/acme/AccountResource.java
+++ code-with-quarkus/src/main/java/org/acme/AccountResource.java
@@ -1,13 +1,7 @@
 package org.acme;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-
-import com.aayushatharva.brotli4j.common.annotations.Local;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -26,19 +20,17 @@ public class AccountResource {
 
     @Inject TransactionRepository transactionRepository;
 
-    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-
     @GET
     @Path("/account/{acc_id}")
     @Produces(MediaType.APPLICATION_JSON)
     public List<AccountTransaction> getAccountTransactions(
         @PathParam("acc_id") String accountId,
-        @QueryParam("status") Optional<Status> status, 
+        @QueryParam("status") Optional<Status> statusOpt, 
         @QueryParam("fromDateTime") Optional<String> fromDateTimeStrOpt,
         @QueryParam("toDateTime") Optional<String> toDateTimeStrOpt) {
 
         return transactionRepository.findByAccountIdStatusAndDatesOptional(
-            accountId, status, fromDateTimeStrOpt, toDateTimeStrOpt);
+            accountId, statusOpt, fromDateTimeStrOpt, toDateTimeStrOpt);
     }
 
 
--- code-with-quarkus/src/main/java/org/acme/TransactionRepository.java
+++ code-with-quarkus/src/main/java/org/acme/TransactionRepository.java
@@ -9,31 +9,12 @@ import java.util.Optional;
 
 import org.acme.AccountTransaction.Status;
 
-import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
 
 @ApplicationScoped
 public class TransactionRepository implements PanacheRepository <AccountTransaction> {
-
-
-    // public List<AccountTransaction> findByAccountIdAndStatus(String accountId, String status) {
-    //     return find( "acc_id=?1 AND status=?2", accountId, status).list();
-    // }
-
-    // public List<AccountTransaction> findByAccountIdAndFromDate(String accountId, String status, 
-    //     LocalDateTime fromDate) {
-    //     return find("acc_id=?1 AND status=?2 AND tx_ts >= ?3", accountId, status, fromDate).list();
-    // }
-
-    // public List<AccountTransaction> findByAccountIdAndFromToDate(String accountId, String status,
-    //         LocalDateTime fromDate, LocalDateTime toDate) {
-    //     return find("acc_id=?1 AND status=?2 AND tx_ts BETWEEN ?3 AND ?4",
-    //      accountId, status, fromDate, toDate).list();
-    // }
-
     
     public List<AccountTransaction> findByAccountIdStatusAndDatesOptional(
         String accountId, Optional<Status> statusOpt, Optional<String> fromDateTimeOpt, 
@@ -59,7 +40,6 @@ public class TransactionRepository implements PanacheRepository <AccountTransact
             queryBuilder.append(" AND transactionDateTime <=:toDateTime");
             params.put("toDateTime", toDateTime);
         }
-        System.out.println(queryBuilder.toString());
         return find(queryBuilder.toString(), params).list();
     }
 
