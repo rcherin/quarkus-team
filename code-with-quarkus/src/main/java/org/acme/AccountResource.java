6207316192bf77e5902df4fa3bcdcb79538103ca
Adam Kaoullas <Adam@Kaoullas.com>
2023-07-23 06:12:19 +0000

    Updated to return Response objects

--- code-with-quarkus/src/main/java/org/acme/AccountResource.java
+++ code-with-quarkus/src/main/java/org/acme/AccountResource.java
@@ -11,6 +11,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import org.acme.AccountTransaction.Status;
 
@@ -23,25 +24,21 @@ public class AccountResource {
     @GET
     @Path("/account/{acc_id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<AccountTransaction> getAccountTransactions(
+    public Response getAccountTransactions(
         @PathParam("acc_id") String accountId,
         @QueryParam("status") Optional<Status> statusOpt, 
         @QueryParam("fromDateTime") Optional<String> fromDateTimeStrOpt,
         @QueryParam("toDateTime") Optional<String> toDateTimeStrOpt) {
 
-        return transactionRepository.findByAccountIdStatusAndDatesOptional(
-            accountId, statusOpt, fromDateTimeStrOpt, toDateTimeStrOpt);
+        return Response.ok(transactionRepository.findByAccountIdStatusAndDatesOptional(
+            accountId, statusOpt, fromDateTimeStrOpt, toDateTimeStrOpt)).build();
     }
 
 
     @GET
     @Path("{tx_id}")
-    public AccountTransaction getByTransactionId(@PathParam("tx_id") String transactionId) {
-        AccountTransaction transaction = AccountTransaction.findById(transactionId);
-        if (transaction == null) {
-            throw new WebApplicationException("Transaction not found", 404);
-        }
-        return transaction;
+    public Response getByTransactionId(@PathParam("tx_id") String transactionId) {
+        return Response.ok(AccountTransaction.findById(transactionId)).build();
     }
     
 }
